d57ec90fd085d3c84984bc8f0a57d23d5d1849b8
surgicoderobot <nabanibanik@yahoo.com>
2024-08-14 15:33:30 +0200

    Update app.py

--- surgicolor/app.py
+++ surgicolor/app.py
@@ -81,7 +81,6 @@ st.write(f"Now, color the selected structure '{st.session_state.current_structur
 
 # Load the third image
 third_image = Image.open(third_image_path)
-print(third_image_path)
 
 # Convert the image to a format suitable for Streamlit canvas
 image_array = np.array(third_image)
