0efbaf5c7122710d0a506b220e5d852c650d1e76
surgicoderobot <nabanibanik@yahoo.com>
2024-08-14 15:12:42 +0200

    Update app.py

--- surgicolor/app.py
+++ surgicolor/app.py
@@ -33,7 +33,7 @@ structures = {
 def find_image_set(structure_name):
     
     archive_path = random.choice(os.listdir('surgicolor/image_data'))
-    print(archive_path, st.session_state.structure_inde)
+    print(archive_path, st.session_state.structure_index)
     full_archive_path = f'image_data/{archive_path}'
     
     # Check if the necessary images exist
